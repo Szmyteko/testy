9cd8201561d7811aa0f08b2842dd2395c0e4e149
Jessie <jakub.stroinski01@gmail.com>
2024-12-01 16:30:30 +0100

    Dodanie widoku dla panelu administratora oraz wyświetlanie w nim użytkowników w aplikacji. Trzeba dodać później CRUD dla tego kontrolera i widoku.
    Utworzono UsersViewModel.cs dla przechowywania informacji o użytkownikach, które później można wyświetlić w widoku panelu administratora.

---
+++ APT-Market/Controllers/AdminPanelController.cs
@@ -0,0 +1,50 @@
+using APT_Market.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APT_Market.Controllers;
+
+[Authorize]
+public class AdminPanelController : Controller
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public AdminPanelController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+
+
+    public async Task<IActionResult> AddRole(string userId, string roleName)
+    {
+        var user = await _userManager.FindByIdAsync(userId);
+        if (user != null && await _roleManager.RoleExistsAsync(roleName))
+        {
+            await _userManager.AddToRoleAsync(user, roleName);
+            return Ok("Rola została dodana.");
+        }
+        return BadRequest("Nie udało się przypisać roli.");
+    }
+    
+    public async Task<IActionResult> UsersWithRoles()
+    {
+        var users = _userManager.Users.ToList();
+        var usersWithRoles = new List<UsersViewModel>();
+
+        foreach (var user in users)
+        {
+            var roles = await _userManager.GetRolesAsync(user);
+            usersWithRoles.Add(new UsersViewModel
+            {
+                UserId = user.Id,
+                Email = user.Email,
+                Roles = roles
+            });
+        }
+
+        return View(usersWithRoles);
+    }
+}
---
+++ APT-Market/Models/UsersViewModel.cs
@@ -0,0 +1,8 @@
+namespace APT_Market.Models;
+
+public class UsersViewModel
+{
+    public string UserId { get; set; }
+    public string Email { get; set; }
+    public IList<string> Roles { get; set; }
+}
